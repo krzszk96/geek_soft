950df4a9a9511a9284a096b8cb8593ab4993b0d2
Krzysiek Szkudlarek <krzszk96@gmail.com>
2026-02-11 16:59:02 +0100

    setup cleanup - ssr & hydration removed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localePl);
+
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [...(appConfig?.providers ?? []), { provide: LOCALE_ID, useValue: 'pl-PL' }],
+}).catch((err) => console.error(err));
